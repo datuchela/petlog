5443de2214b0ac6ab6d47f0b061982bcbdbe7277
datuchela <davit.narimanidze408@gmail.com>
2022-11-11 18:45:07 +0400

    added Date table - many to one

--- server/prisma/migrations/20221111144417_/migration.sql
+++ server/prisma/migrations/20221111144417_/migration.sql
@@ -15,16 +15,22 @@ CREATE TABLE "RefreshToken" (
 );
 
 -- CreateTable
+CREATE TABLE "Date" (
+    "date" TEXT NOT NULL
+);
+
+-- CreateTable
 CREATE TABLE "Reminder" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "upcoming" TEXT NOT NULL,
-    "intervalValue" TEXT NOT NULL,
+    "dateString" TEXT NOT NULL,
+    "intervalValue" INTEGER NOT NULL,
     "intervalType" TEXT NOT NULL,
     "petId" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
-    CONSTRAINT "Reminder_petId_fkey" FOREIGN KEY ("petId") REFERENCES "Pet" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Reminder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Reminder_dateString_fkey" FOREIGN KEY ("dateString") REFERENCES "Date" ("date") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Reminder_petId_fkey" FOREIGN KEY ("petId") REFERENCES "Pet" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Reminder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -33,11 +39,11 @@ CREATE TABLE "Pet" (
     "name" TEXT NOT NULL,
     "gender" TEXT NOT NULL,
     "birthday" TEXT NOT NULL,
-    "weight" TEXT NOT NULL,
+    "weight" TEXT,
     "ownerId" INTEGER NOT NULL,
     "speciesId" INTEGER NOT NULL,
-    CONSTRAINT "Pet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Pet_speciesId_fkey" FOREIGN KEY ("speciesId") REFERENCES "Species" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "Pet_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Pet_speciesId_fkey" FOREIGN KEY ("speciesId") REFERENCES "Species" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -54,3 +60,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "RefreshToken_token_key" ON "RefreshToken"("token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Date_date_key" ON "Date"("date");
