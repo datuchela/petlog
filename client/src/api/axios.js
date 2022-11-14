d9c356f5467365f2d2970694e68e9e4203ee95ee
datuchela <davit.narimanidze408@gmail.com>
2022-11-15 02:05:25 +0400

    changed prod BASE_URL to api.datuchela.com

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.DEV
   ? "http://localhost:5050"
-  : "https://api.petlog.datuchela.com";
+  : "https://api.datuchela.com";
 
 export default axios.create({
   baseURL: BASE_URL,
